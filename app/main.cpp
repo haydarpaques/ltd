aa03c23d0e5c48a596649dbffc761171ac084b18
Benni Adham <benni.adham@gmail.com>
2021-05-14 10:37:00 +0700

    Split cmake declares

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,12 @@
-#include <regex>
-
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
 #include <stdio.h>
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
 
 #include <ltd.h>
 #include <ltd_cfg.h>
@@ -64,6 +67,124 @@ void print_version()
     fmt::println("ltd version %d.%d", LTD_VERSION_MAJOR, LTD_VERSION_MINOR);
 }
 
+error config_lib(const std::string &home_path, const std::string &project_name, const std::string &lib_name, const std::string &path, std::string &cmake_text)
+{
+    // Create the section
+    cmake_text += fmt::sprintln("\n# configure 'lib%s.a' target", lib_name);
+
+    // Collect source files
+    std::string src_path = home_path + project_name + "/" + path;
+    std::string files;
+    for (const auto& dir_entry : fs::directory_iterator(src_path))
+    {
+        if (fs::is_directory(dir_entry) == true)
+                continue;
+
+        if (dir_entry.path().extension() != ".cpp" &&
+            dir_entry.path().extension() != ".cxx" &&
+            dir_entry.path().extension() != ".cc") {
+                continue;
+            }
+
+        files += path + "/" + dir_entry.path().filename().string();
+        files += " ";
+    }
+
+    if (files.length() == 0)
+        return error::not_found;
+
+    cmake_text += fmt::sprintln("add_library( %s-lib %s)", lib_name, files);
+    cmake_text += fmt::sprintln("target_include_directories( %s-lib PUBLIC \"${PROJECT_BINARY_DIR}\" )", lib_name);
+    cmake_text += fmt::sprintln("target_include_directories( %s-lib PUBLIC \"${PROJECT_SOURCE_DIR}/inc/\" )", lib_name);
+    cmake_text += fmt::sprintln("set_target_properties( %s-lib PROPERTIES OUTPUT_NAME %s )", lib_name, lib_name);
+    cmake_text += "\n";
+
+    return error::no_error;
+}
+
+error config_app(const std::string &home_path, const std::string &project_name, const std::string &app_name, const std::string &path, const std::vector<std::string> &project_libs, std::string &cmake_text)
+{
+    // Create the section
+    cmake_text += fmt::sprintln("\n# configure '%s' executable target", app_name);
+
+    // Collect source files
+    std::string src_path = home_path + project_name + "/" + path;
+    std::string files;
+    for (const auto& dir_entry : fs::directory_iterator(src_path))
+    {
+        if (fs::is_directory(dir_entry) == true)
+                continue;
+
+        if (dir_entry.path().extension() != ".cpp" &&
+            dir_entry.path().extension() != ".cxx" &&
+            dir_entry.path().extension() != ".cc") {
+                continue;
+            }
+
+        files += path + "/" + dir_entry.path().filename().string();
+        files += " ";
+    }
+
+    if (files.length() == 0)
+        return error::not_found;
+
+    cmake_text += fmt::sprintln("add_executable( %s-exe %s)", app_name, files);
+    cmake_text += fmt::sprintln("target_include_directories( %s-exe PUBLIC \"${PROJECT_BINARY_DIR}\")", app_name);
+    cmake_text += fmt::sprintln("target_include_directories( %s-exe PUBLIC \"${PROJECT_SOURCE_DIR}/inc/\")", app_name);
+    cmake_text += fmt::sprintln("set_target_properties( %s-exe PROPERTIES OUTPUT_NAME %s )", app_name, app_name);
+
+    std::string local_libs;
+    for (auto lib_target : project_libs) {
+        local_libs += lib_target + "-lib ";
+    }
+
+    cmake_text += fmt::sprintln("target_link_libraries( %s-exe %s stdc++fs)", app_name, local_libs );
+    cmake_text += "\n";
+
+    return error::no_error;
+}
+
+bool is_project_dirty(const std::string &home_path, const std::string &project_name)
+{
+    // We need to generate one when the following conditions met:
+    // - The file is not there
+    // - The file is older than the any of the folders in the project.
+    if ( fs::exists(home_path + "/" + project_name + "/CMakeLists.txt") == false )
+        return true;
+
+    auto cmake_list_ts = fs::last_write_time( home_path + "/" + project_name + "/CMakeLists.txt" );
+    auto folders_ts = fs::last_write_time( home_path + "/" + project_name );
+
+    if (folders_ts > cmake_list_ts) 
+        return true;
+
+    if (fs::exists(home_path + "/" + project_name + "/app")) {
+        folders_ts = fs::last_write_time( home_path + "/" + project_name + "/app" );
+        if (folders_ts > cmake_list_ts) 
+            return true;
+    }
+
+    if (fs::exists(home_path + "/" + project_name + "/apps")) {
+        folders_ts = fs::last_write_time( home_path + "/" + project_name + "/apps" );
+        if (folders_ts > cmake_list_ts) 
+            return true;
+    }
+
+    if (fs::exists(home_path + "/" + project_name + "/lib")) {
+        folders_ts = fs::last_write_time( home_path + "/" + project_name + "/lib" );
+        if (folders_ts > cmake_list_ts) 
+            return true;
+    }
+
+    if (fs::exists(home_path + "/" + project_name + "/libs")) {
+        folders_ts = fs::last_write_time( home_path + "/" + project_name + "/libs" );
+        if (folders_ts > cmake_list_ts) 
+            return true;
+    }
+
+    return false;    
+}
+
 int main(int argc, char *argv[])
 {
     std::string home_path;
@@ -72,7 +193,7 @@ int main(int argc, char *argv[])
     flags.init(argc, argv);
 
     // Bind arguments
-    flags.bind(opt_verbosity, 0, 'v', "verbose", "Verbose logging.");
+    flags.bind(opt_verbosity, 0, 'v', "verbose", "logging verbosity.");
 
     if (flags.size() < 2) {
         print_help(flags); 
@@ -121,6 +242,85 @@ int main(int argc, char *argv[])
 
         if (check_version("Git", "the stupid content tracker", "git --version", "([0-9]+\\.[0-9]+\\.[0-9]+)") != error::no_error)
             return -1;
+
+        if (command == "build") {
+
+            std::vector<std::string> project_libs;
+
+            // Reading the project name and validating
+            if (flags.size() < 3) {
+                fmt::println("Expecting project name");
+                print_help(flags);
+                return -1;
+            }
+
+            auto [project_name, err0] = flags.at(2);
+            if (err0 != error::no_error) {
+                fmt::println("Error while retrieving project name. Exiting...");
+                return -1;
+            }
+
+            // Check whether we need to generate new CMakeLists.txt           
+            if (is_project_dirty(home_path, project_name)) {
+                std::string cmake_txt;
+                cmake_txt += fmt::sprintln("cmake_minimum_required(VERSION 3.10)");
+                cmake_txt += fmt::sprintln("");
+                cmake_txt += fmt::sprintln("project (%s)", project_name);
+                cmake_txt += fmt::sprintln("");
+                cmake_txt += fmt::sprintln("set(CMAKE_CXX_STANDARD 17)");
+                cmake_txt += fmt::sprintln("set(CMAKE_CXX_STANDARD_REQUIRED True)");
+
+                // Libraries section
+                // Determine whether we have a single library build or multiple
+                if  (fs::exists(home_path + project_name + "/lib")) {
+                    fmt::println("Building 'lib%s.a'", project_name);
+                    config_lib(home_path, project_name, project_name, "lib", cmake_txt);
+                    project_libs.push_back(project_name);
+                }
+
+                if  (fs::exists(home_path + project_name + "/libs")) {
+                    for (const auto& dir_entry : fs::directory_iterator(home_path + project_name + "/libs")) {
+                        if (fs::is_directory(dir_entry) == true) {
+                            std::string lib_name = dir_entry.path().filename().string();
+                            std::string lib_path = "libs/" + lib_name;
+
+                            config_lib( home_path, project_name, lib_name, lib_path, cmake_txt );
+                            project_libs.push_back(lib_name);
+                        }
+                    }
+                }
+
+                // Apps section
+                // Determine whether we have a single app build or multiple
+                if  (fs::exists(home_path + project_name + "/app")) {
+                    fmt::println("Building 'app' executable", project_name);
+                    config_app(home_path, project_name, project_name, "app", project_libs, cmake_txt);
+                }
+
+                if  (fs::exists(home_path + project_name + "/apps")) {
+                    for (const auto& dir_entry : fs::directory_iterator(home_path + project_name + "/apps")) {
+                        if (fs::is_directory(dir_entry) == true) {
+                            std::string app_name = dir_entry.path().filename().string();
+                            std::string app_path = "apps/" + app_name;
+
+                            config_app( home_path, project_name, app_name, app_path, project_libs, cmake_txt );
+                        }
+                    }
+                }
+    
+                if ( fs::exists(home_path + "/" + project_name + "/CMakeLists.txt") )
+                    fs::remove( home_path + "/" + project_name + "/CMakeLists.txt" );
+
+                // fmt::println("%s", cmake_txt);
+
+                std::ofstream out(home_path + "/" + project_name + "/CMakeLists.txt");
+                out << cmake_txt << std::endl;
+                out.close();
+            }
+        } else {
+            fmt::println("Command '%s' is not recognized...", command);
+            print_help(flags);
+        }
     }
  
     return 0;
