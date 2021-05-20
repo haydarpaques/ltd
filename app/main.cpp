e459e723bec85836db1918f4943dccadff84a9db
Benni Adham <benni.adham@gmail.com>
2021-05-20 09:43:42 +0700

    Refactor build and clean commands

--- app/main.cpp
+++ app/main.cpp
@@ -49,7 +49,7 @@ error check_version(const char *name, const char *desc, const char *cmd, const c
     return error::not_found;
 }
 
-void print_help(args_opt& opts)
+void print_help(const args_opt& opts)
 {
     fmt::println("Usage: ltd <command> [options...]");
     fmt::println("");
@@ -185,6 +185,141 @@ bool is_project_dirty(const std::string &home_path, const std::string &project_n
     return false;    
 }
 
+error clean(const args_opt& flags, const std::string &home_path) 
+{
+    // Reading the project name and validating
+    if (flags.size() < 3) {
+        fmt::println("Expecting project name");
+        print_help(flags);
+        return error::invalid_argument;
+    }
+
+    auto [project_name, err0] = flags.at(2);
+    if (err0 != error::no_error) {
+        fmt::println("Error while retrieving project name. Exiting...");
+        return error::invalid_argument;
+    }
+
+    // Check apps name
+    if (project_name.find('/') != std::string::npos) {
+        if (fs::exists(home_path+project_name+"app") ) {
+             // Check multiple apps binaries
+        }
+    } else {
+        if (fs::exists(home_path+project_name+"app") ) {
+             // Check single app binary
+        }
+    }
+
+    return error::no_error;
+}
+
+error clean(const args_opt& flags, const std::string &home_path) 
+{
+    // Reading the project name and validating
+    if (flags.size() < 3) {
+        fmt::println("Expecting project name");
+        print_help(flags);
+        return error::invalid_argument;
+    }
+
+    auto [project_name, err0] = flags.at(2);
+    if (err0 != error::no_error) {
+        fmt::println("Error while retrieving project name. Exiting...");
+        return error::invalid_argument;
+    }
+
+    if (fs::exists(home_path + "caches/" + project_name))
+        fs::remove_all(home_path + "caches/" + project_name);  
+
+    return error::no_error;          
+}
+
+error build(const args_opt& flags, const std::string &home_path)
+{
+    std::vector<std::string> project_libs;
+
+    // Reading the project name and validating
+    if (flags.size() < 3) {
+        fmt::println("Expecting project name");
+        print_help(flags);
+        return error::invalid_argument;
+    }
+
+    auto [project_name, err0] = flags.at(2);
+    if (err0 != error::no_error) {
+        fmt::println("Error while retrieving project name. Exiting...");
+        return error::invalid_argument;
+    }
+
+    // Check whether we need to generate new CMakeLists.txt           
+    if (is_project_dirty(home_path, project_name)) {
+        std::string cmake_txt;
+        cmake_txt += fmt::sprintln("cmake_minimum_required(VERSION 3.10)");
+        cmake_txt += fmt::sprintln("");
+        cmake_txt += fmt::sprintln("project (%s)", project_name);
+        cmake_txt += fmt::sprintln("");
+        cmake_txt += fmt::sprintln("set(CMAKE_CXX_STANDARD 17)");
+        cmake_txt += fmt::sprintln("set(CMAKE_CXX_STANDARD_REQUIRED True)");
+
+        // Libraries section
+        // Determine whether we have a single library build or multiple
+        if  (fs::exists(home_path + project_name + "/lib")) {
+            fmt::println("Building 'lib%s.a'", project_name);
+            config_lib(home_path, project_name, project_name, "lib", cmake_txt);
+            project_libs.push_back(project_name);
+        }
+
+        if  (fs::exists(home_path + project_name + "/libs")) {
+            for (const auto& dir_entry : fs::directory_iterator(home_path + project_name + "/libs")) {
+                if (fs::is_directory(dir_entry) == true) {
+                    std::string lib_name = dir_entry.path().filename().string();
+                    std::string lib_path = "libs/" + lib_name;
+
+                    config_lib( home_path, project_name, lib_name, lib_path, cmake_txt );
+                    project_libs.push_back(lib_name);
+                }
+            }
+        }
+
+        // Apps section
+        // Determine whether we have a single app build or multiple
+        if  (fs::exists(home_path + project_name + "/app")) {
+            fmt::println("Building 'app' executable", project_name);
+            config_app(home_path, project_name, project_name, "app", project_libs, cmake_txt);
+        }
+
+        if  (fs::exists(home_path + project_name + "/apps")) {
+            for (const auto& dir_entry : fs::directory_iterator(home_path + project_name + "/apps")) {
+                if (fs::is_directory(dir_entry) == true) {
+                    std::string app_name = dir_entry.path().filename().string();
+                    std::string app_path = "apps/" + app_name;
+
+                    config_app( home_path, project_name, app_name, app_path, project_libs, cmake_txt );
+                }
+            }
+        }
+
+        if ( fs::exists(home_path + "/" + project_name + "/CMakeLists.txt") )
+            fs::remove( home_path + "/" + project_name + "/CMakeLists.txt" );
+
+        fmt::println("-- Writing CMakeLists.txt to '%s'", home_path + project_name);
+
+        std::ofstream out(home_path + "/" + project_name + "/CMakeLists.txt");
+        out << cmake_txt << std::endl;
+        out.close();
+    }
+
+    if (fs::exists(home_path + "caches/" + project_name) == false)
+        fs::create_directory(home_path + "caches/" + project_name);
+
+    fs::current_path(home_path + "caches/" + project_name);
+    std::system(fmt::sprintf("cmake ../../%s .", project_name).c_str());
+    std::system("cmake --build .");
+
+    return error::no_error;
+}
+
 int main(int argc, char *argv[])
 {
     std::string home_path;
@@ -241,105 +376,21 @@ int main(int argc, char *argv[])
         if (check_version("Git", "the stupid content tracker", "git --version", "([0-9]+\\.[0-9]+\\.[0-9]+)") != error::no_error)
             return -1;
 
-        if (command == "build") {
+        if (command == "build") 
+        {
+            auto err = build(flags, home_path);
 
-            std::vector<std::string> project_libs;
-
-            // Reading the project name and validating
-            if (flags.size() < 3) {
-                fmt::println("Expecting project name");
-                print_help(flags);
+            if (err != error::no_error) {
                 return -1;
-            }
-
-            auto [project_name, err0] = flags.at(2);
-            if (err0 != error::no_error) {
-                fmt::println("Error while retrieving project name. Exiting...");
-                return -1;
-            }
-
-            // Check whether we need to generate new CMakeLists.txt           
-            if (is_project_dirty(home_path, project_name)) {
-                std::string cmake_txt;
-                cmake_txt += fmt::sprintln("cmake_minimum_required(VERSION 3.10)");
-                cmake_txt += fmt::sprintln("");
-                cmake_txt += fmt::sprintln("project (%s)", project_name);
-                cmake_txt += fmt::sprintln("");
-                cmake_txt += fmt::sprintln("set(CMAKE_CXX_STANDARD 17)");
-                cmake_txt += fmt::sprintln("set(CMAKE_CXX_STANDARD_REQUIRED True)");
-
-                // Libraries section
-                // Determine whether we have a single library build or multiple
-                if  (fs::exists(home_path + project_name + "/lib")) {
-                    fmt::println("Building 'lib%s.a'", project_name);
-                    config_lib(home_path, project_name, project_name, "lib", cmake_txt);
-                    project_libs.push_back(project_name);
-                }
-
-                if  (fs::exists(home_path + project_name + "/libs")) {
-                    for (const auto& dir_entry : fs::directory_iterator(home_path + project_name + "/libs")) {
-                        if (fs::is_directory(dir_entry) == true) {
-                            std::string lib_name = dir_entry.path().filename().string();
-                            std::string lib_path = "libs/" + lib_name;
-
-                            config_lib( home_path, project_name, lib_name, lib_path, cmake_txt );
-                            project_libs.push_back(lib_name);
-                        }
-                    }
-                }
-
-                // Apps section
-                // Determine whether we have a single app build or multiple
-                if  (fs::exists(home_path + project_name + "/app")) {
-                    fmt::println("Building 'app' executable", project_name);
-                    config_app(home_path, project_name, project_name, "app", project_libs, cmake_txt);
-                }
-
-                if  (fs::exists(home_path + project_name + "/apps")) {
-                    for (const auto& dir_entry : fs::directory_iterator(home_path + project_name + "/apps")) {
-                        if (fs::is_directory(dir_entry) == true) {
-                            std::string app_name = dir_entry.path().filename().string();
-                            std::string app_path = "apps/" + app_name;
-
-                            config_app( home_path, project_name, app_name, app_path, project_libs, cmake_txt );
-                        }
-                    }
-                }
-    
-                if ( fs::exists(home_path + "/" + project_name + "/CMakeLists.txt") )
-                    fs::remove( home_path + "/" + project_name + "/CMakeLists.txt" );
-
-                fmt::println("-- Writing CMakeLists.txt to '%s'", home_path + project_name);
-
-                std::ofstream out(home_path + "/" + project_name + "/CMakeLists.txt");
-                out << cmake_txt << std::endl;
-                out.close();
-            }
-
-            if (fs::exists(home_path + "caches/" + project_name) == false)
-                fs::create_directory(home_path + "caches/" + project_name);
-
-            fs::current_path(home_path + "caches/" + project_name);
-            std::system(fmt::sprintf("cmake ../../%s .", project_name).c_str());
-            std::system("cmake --build .");
+            }           
         } // 'build' command
         else if (command == "clean")
         {
-            // Reading the project name and validating
-            if (flags.size() < 3) {
-                fmt::println("Expecting project name");
-                print_help(flags);
+            auto err = clean(flags, home_path);
+
+            if (err != error::no_error) {
                 return -1;
             }
-
-            auto [project_name, err0] = flags.at(2);
-            if (err0 != error::no_error) {
-                fmt::println("Error while retrieving project name. Exiting...");
-                return -1;
-            }
-
-            if (fs::exists(home_path + "caches/" + project_name))
-                fs::remove_all(home_path + "caches/" + project_name);            
         } // 'clean' command
         else 
         {
